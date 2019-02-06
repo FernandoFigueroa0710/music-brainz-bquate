4066663029a79432b24e284ef159d9c17fb3142d
FernandoFfigueroa0710 <fernandoffigueroa@gmail.com>
2019-02-05 19:03:09 -0600

    added tachyons styles to profile

--- src/components/artist/artist_profile.js
+++ src/components/artist/artist_profile.js
@@ -2,15 +2,15 @@ import React from "react";
 
 const ArtistProfile = ({ id, album, name, score, description }) => {
   return (
-    <div>
+    <div className="bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5">
       <h3>{name}</h3>
       <img
         alt="artist"
         src={`https://coverartarchive.org/release/${album}/front`}
       />
-      <h3>{score}</h3>
+      <h3>Socore:{score}</h3>
       <br />
-      <h2>{description}</h2>
+      <h2>Description:{description}</h2>
     </div>
   );
 };
