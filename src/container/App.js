92fa1756b64020cacef8b4bd40c3b122ea3925cb
FernandoFfigueroa0710 <fernandoffigueroa@gmail.com>
2019-02-05 18:45:19 -0600

    set up API query and retrieving data from musicbrainz

--- src/container/App.js
+++ src/container/App.js
@@ -1,29 +1,37 @@
 import React, { Component } from "react";
 import "./App.css";
-
+import ArtistList from "../components/artist/artist_list";
 class App extends Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
-      artists: {}
+      artistslist: [],
+      albumlist: []
     };
   }
 
   componentDidMount() {
     this.getArtists();
+    //this.getAlbums();
   }
-
+  // getAlbums() {
+  //   fetch("https://coverartarchive.org/release/{mbid}/front")
+  //     .then(response => response.json())
+  //     .then(data => this.setState({ albumlist: data.images }));
+  // }
   getArtists() {
     fetch("http://musicbrainz.org/ws/2/artist?query=artists&limit=25&fmt=json")
       .then(response => response.json())
-      .then(artists => this.setState({ artists: artists }));
+      .then(data => this.setState({ artistslist: data.artists }));
   }
+
   render() {
-    console.log(this.state.artists);
+    console.log(this.state.artistslist);
     return (
       <div className="App">
         <header>
-          <h1>This is the parent component</h1>
+          <h1>Welcome to musicbrainz</h1>
+          <ArtistList artistList={this.state.artistslist} />
         </header>
       </div>
     );
