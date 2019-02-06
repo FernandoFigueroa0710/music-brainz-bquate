1781702e1f26348d6c7251685d11352471b19443
FernandoFfigueroa0710 <fernandoffigueroa@gmail.com>
2019-02-05 18:44:16 -0600

    update: set up artist profile component

--- src/components/artist/artist_profile.js
+++ src/components/artist/artist_profile.js
@@ -1,6 +1,17 @@
 import React from "react";
 
-const ArtistProfile = () => {
-  return <div />;
+const ArtistProfile = ({ id, album, name, score, description }) => {
+  return (
+    <div>
+      <h3>{name}</h3>
+      <img
+        alt="artist"
+        src={`https://coverartarchive.org/release/${album}/front`}
+      />
+      <h3>{score}</h3>
+      <br />
+      <h2>{description}</h2>
+    </div>
+  );
 };
 export default ArtistProfile;
