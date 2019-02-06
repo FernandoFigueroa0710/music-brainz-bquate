24d4d512ea95ac028b4920751536c04fb6a056bb
FernandoFfigueroa0710 <fernandoffigueroa@gmail.com>
2019-02-05 18:42:56 -0600

    update: set up artist list properties

--- src/components/artist/artist_list.js
+++ src/components/artist/artist_list.js
@@ -1,7 +1,23 @@
 import React from "react";
+import ArtistProfile from "./artist_profile";
 
-const ArtistList = () => {
-  return <div />;
+const ArtistList = ({ artistList }) => {
+  return (
+    <div>
+      {artistList.map((artist, i) => {
+        return (
+          <ArtistProfile
+            key={i}
+            id={artist.id}
+            name={artist.name}
+            album={artist["type-id"]}
+            score={artist.score}
+            description={artist.disambiguation}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default ArtistList;
